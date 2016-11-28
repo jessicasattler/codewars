6413e666b55eccf0b1198e043fb73afdc194ae6b
Jessica Sattler <jessicasattler28@gmail.com>
2016-11-28 11:34:04 -0600

    Set up instructions on how to succesfully link up a repository from laptop to github

---
+++ creatingNewRepository.php
@@ -0,0 +1,12 @@
+Creating new repository:
+
+1. make new folder 
+2. navigate to that folder from the command line 
+3. run "git init"
+4. create a file for the intial commit
+5. go to github and click on "New Repository"
+6. give it a descriptive name
+7. after clicking "create new repository", 
+8. focus on the section that says:"…or push an existing repository from the command line" and copy and paste the command that starts with : git push -u origin master .. 
+9. then do the git status, git commit -A, and then the "git push -u origin master" for the first commit
+10.Enjoy!
