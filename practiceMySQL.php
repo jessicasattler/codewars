4fc0297039097ed4e05131810d74325480439b99
Jessica Sattler <jessicasattler28@gmail.com>
2016-11-29 12:07:51 -0600

    Solution to query by id

--- practiceMySQL.php
+++ practiceMySQL.php
@@ -22,3 +22,8 @@ Query all columns (attributes) for every row in the CITY table.
 Solution:
 
 SELECT * FROM city;
+
+#Select by id
+Query all columns for a city in CITY with the ID 1661
+
+SELECT * FROM city WHERE id = 1661;
