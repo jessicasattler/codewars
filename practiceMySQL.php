a03fc11e1338eb1410b5c7871a287dbf8109902d
Jessica Sattler <jessicasattler28@gmail.com>
2016-11-29 11:57:01 -0600

    Solution to Select Query II

--- practiceMySQL.php
+++ practiceMySQL.php
@@ -1,6 +1,6 @@
-solutions to HackerRank MySQL Challenges:
+Solutions to HackerRank MySQL Challenges:
 
-
+#Practice with the Select Query I
 Query all columns for all American cities in CITY with populations larger than 100000. The CountryCode for America is USA.
 
 link: https://www.hackerrank.com/challenges/revising-the-select-query
@@ -8,3 +8,10 @@ link: https://www.hackerrank.com/challenges/revising-the-select-query
 Solution:
 
 SELECT * FROM city WHERE countrycode = 'USA' AND population > 100000;
+
+#Revising the Select Query II
+
+Query the names of all American cities in CITY with populations larger than 120000. The CountryCode for America is USA.
+
+Solution:
+SELECT name FROM city WHERE countrycode = 'USA' AND population > 120000;
