a7bff51ed070039c6b7cc6b7498d8a5830072726
Jessica Sattler <jessicasattler28@gmail.com>
2016-12-06 14:36:06 -0600

    Find all the cities that don't start with any of the vowels

--- practiceMySQL.php
+++ practiceMySQL.php
@@ -75,3 +75,8 @@ SELECT DISTINCT city FROM station WHERE city LIKE '%a' OR city LIKE '%e' OR city
 Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
 
 select distinct CITY from STATION where substr(CITY,1,1) in ('A','E','I','O','U') and substr(CITY,-1,1) in ('A','E','I','O','U');
+
+#weather observation station 9
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT city FROM station WHERE city NOT LIKE 'a%' AND city NOT LIKE 'e%' AND city NOT LIKE 'i%' AND city NOT LIKE 'o%' AND city NOT LIKE 'u%';
