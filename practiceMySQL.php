fd23623e21c65095ba42c8c62de676b95ba5003d
Jessica Sattler <jessicasattler28@gmail.com>
2016-12-06 14:08:07 -0600

    Find the distint cities that end with any of the vowels

--- practiceMySQL.php
+++ practiceMySQL.php
@@ -65,3 +65,8 @@ SELECT city, CHAR_LENGTH(city) FROM station ORDER BY CHAR_LENGTH(city) desc, cit
 Query the list of CITY names starting with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
 
 SELECT DISTINCT city FROM station WHERE city LIKE 'a%' OR city LIKE 'e%' OR city LIKE 'i%' OR city LIKE 'o%'OR city LIKE'u%';
+
+#weather observation station 7
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+SELECT DISTINCT city FROM station WHERE city LIKE '%a' OR city LIKE '%e' OR city LIKE '%i' OR city LIKE '%o' OR city LIKE '%u';
